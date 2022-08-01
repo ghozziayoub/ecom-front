c65ec954e6354bfc0bbae300d5b9361aef8f0513
Ayoub Ghozzi <ghozziayoub@gmail.com>
2022-08-01 11:18:25 +0100

    init home and 404 pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/public/home/home.component';
+import { Page404Component } from './components/public/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "**",
+    component: Page404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
